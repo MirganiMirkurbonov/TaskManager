48ef2fe4f70d317725616f0af60400e6058666c1
mirgani <mirganimirkurbonov@gmail.com>
2023-09-26 22:00:09 +0500

    Initial commit

---
+++ Domain/Schemas/Auth/AuthUser.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Schemas.BaseEntity;
+
+namespace Domain.Schemas.Auth;
+
+public class AuthUser : Entity
+{
+    [Column("first_name")]
+    public string FirstName { get; set; } = null!;
+    
+    [Column("last_name")]
+    public string? LastName { get; set; }
+
+    [Column("username")]
+    public string Username { get; set; } = null!;
+
+    [Column("password_hash")]
+    public string PasswordHash { get; set; } = null!;
+
+    [Column("password_salt")]
+    public string PasswordSalt { get; set; } = null!;
+}
---
+++ Domain/Schemas/BaseEntity/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Enumerators;
+
+namespace Domain.Schemas.BaseEntity;
+
+public abstract class Entity
+{
+    [Column("id")]
+    public long Id { get; set; }
+    
+    [Column("state")]
+    public EState State { get; set; }
+}
